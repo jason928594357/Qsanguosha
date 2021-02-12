7d6770a6d49377b6a611b9de83e04a61267f79df
jason928594357 <928594357@qq.com>
2021-02-12 19:01:14 +0800

    添加工具类

---
+++ src/core/util.h
@@ -0,0 +1,27 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+class QVariant;
+#include <QList>
+#include <QStringList>
+#include <QVariant>
+
+template<typename T>
+void qShuffle(QList<T> &list){
+    int n =list.length();
+    for(int i=0;i<n;i++){
+        int r= qrand() % (n-i)+i;
+        list.swap(i,r);
+    }
+}
+
+QStringList IntList2StringList(const QList<int> &intlist);
+QList<int> StringList2IntList(const QStringList &stringlist);
+QVariantList IntList2VariantList(const QList<int> &intlist);
+QList<int> VariantList2IntList(const QVariantList &variantlist);
+
+bool isNormalGameMode(const QString &mode);
+
+static const int S_EQUIP_AREA_LENGTH = 5;
+
+#endif // UTIL_H
