7b853f0e2cfc27ca1aedee3bf9ab7136cb9937ee
jason928594357 <928594357@qq.com>
2021-02-17 16:23:54 +0800

    connectiondialog.cpp

--- src/dialog/connectiondialog.h
+++ src/dialog/connectiondialog.h
@@ -16,12 +16,17 @@ class ConnectionDialog : public FlatDialog
 public:
     ConnectionDialog(QWidget *parent);
     ~ConnectionDialog();
-
+    void hideAvatarList();
+    void showAvatarList();
 private:
     Ui::ConnectionDialog *ui;
 
 private slots:
-
+    void on_detectLANButton_clicked();
+    void on_clearHistoryButton_clicked();
+    void on_avatarList_doubleClicked(const QModelIndex &index);
+    void on_changeAvatarButton_clicked();
+    void on_connectButton_clicked();
 };
 
 #endif // CONNECTIONDIALOG_H
