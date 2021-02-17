3e12275bf373f8aeb32f0597b0f1737aae4e443d
jason928594357 <928594357@qq.com>
2021-02-17 15:16:35 +0800

    Setting配置文件

--- src/core/settings.cpp
+++ src/core/settings.cpp
@@ -1,5 +1,9 @@
 #include "settings.h"
 
+#include <QApplication>
+
+Settings *SettingsInstance = NULL;
+
 static const qreal ViewWidth = 1280 * 0.8;
 static const qreal ViewHeight = 800 * 0.8;
 
@@ -11,5 +15,7 @@ Settings::Settings()
 #endif
     Rect(-ViewWidth / 2, -ViewHeight / 2, ViewWidth, ViewHeight)
 {
-
+    Q_ASSERT(SettingsInstance == NULL);
+    SettingsInstance = this;
+    connect(qApp, &QApplication::aboutToQuit, this, &Settings::deleteLater);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-#include "mainwindow.h"
+#include <QCoreApplication>
 #include <QApplication>
+
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+    QApplication a(argc, argv);
+    MainWindow main_window;
+    main_window.show();
+
+    return qApp->exec();
 }
