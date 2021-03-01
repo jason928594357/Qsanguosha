7e16e89305fa40f5d6a4c2b58d65750250199fee
jason928594357 <928594357@qq.com>
2021-03-01 19:36:47 +0800

    提交翻译

--- builds/sanguosha.ts
+++ builds/sanguosha.ts
@@ -2,10 +2,68 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>CardOverview</name>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="14"/>
+        <source>Card Overivew</source>
+        <translation>卡牌一览</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="39"/>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="44"/>
+        <source>Suit</source>
+        <translation>花色</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="49"/>
+        <source>Point</source>
+        <translation>点数</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="54"/>
+        <source>Type</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="59"/>
+        <source>Subtype</source>
+        <translation>子类型</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="64"/>
+        <source>Package</source>
+        <translation>包</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="127"/>
+        <source>Play (Male)</source>
+        <translation>男性配音</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="140"/>
+        <source>Play (Female)</source>
+        <translation>女性配音</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="149"/>
+        <source>Play audio effect</source>
+        <translation>音效</translation>
+    </message>
+    <message>
+        <location filename="../src/dialog/cardoverview.ui" line="162"/>
+        <source>Get this card</source>
+        <translation>获得卡牌</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="../src/dialog/mainwindow.ui" line="14"/>
-        <location filename="../src/dialog/mainwindow.cpp" line="87"/>
+        <location filename="../src/dialog/mainwindow.cpp" line="88"/>
         <source>Sanguosha</source>
         <translation>三国杀</translation>
     </message>
@@ -25,72 +83,72 @@
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="63"/>
+        <location filename="../src/dialog/mainwindow.ui" line="56"/>
         <source>Start Game</source>
         <translation>启动游戏</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="66"/>
+        <location filename="../src/dialog/mainwindow.ui" line="59"/>
         <source>Ctrl+G</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="71"/>
+        <location filename="../src/dialog/mainwindow.ui" line="64"/>
         <source>Start Server</source>
         <translation>启动服务器</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="74"/>
+        <location filename="../src/dialog/mainwindow.ui" line="67"/>
         <source>Ctrl+S</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="79"/>
+        <location filename="../src/dialog/mainwindow.ui" line="72"/>
         <source>Replay</source>
         <translation>重放战局</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="84"/>
+        <location filename="../src/dialog/mainwindow.ui" line="77"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="89"/>
+        <location filename="../src/dialog/mainwindow.ui" line="82"/>
         <source>Configure</source>
         <translation>配置</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="94"/>
+        <location filename="../src/dialog/mainwindow.ui" line="87"/>
         <source>General overview</source>
         <translation>武将一览</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="97"/>
+        <location filename="../src/dialog/mainwindow.ui" line="90"/>
         <source>Ctrl+E</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="102"/>
+        <location filename="../src/dialog/mainwindow.ui" line="95"/>
         <source>Card overview</source>
         <translation>卡牌一览</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="105"/>
+        <location filename="../src/dialog/mainwindow.ui" line="98"/>
         <source>Ctrl+R</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="110"/>
+        <location filename="../src/dialog/mainwindow.ui" line="103"/>
         <source>Scenario overview</source>
         <translation>场景一览</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.ui" line="115"/>
+        <location filename="../src/dialog/mainwindow.ui" line="108"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../src/dialog/mainwindow.cpp" line="88"/>
+        <location filename="../src/dialog/mainwindow.cpp" line="89"/>
         <source>Are you sure to exit?</source>
         <translation>你确定要退出吗？</translation>
     </message>
